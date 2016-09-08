480e676ba9d91c898448848ddc7fa40ce0cca680
Александр Некрасов <lxmrrr@gmail.com>
2016-09-08 17:57:45 +0300

    features+advantages

---
+++ page-home.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+<div class="content-area">
+	<main class="site-main home">
+		<section class="features">
+			<div class="features__wrapper lcw">
+				<div class="features__item">
+					<a href="#" class="features__inner">
+						<h2 class="features__header">Проектирование металлических конструкций</h2>
+						<p class="features__text">Проектирование и изготовление металлоконструкций обязательно потребуется при создании сложных и уникальных объектов: небоскребов, зданий аэропортов и вокзалов, крытых стадионов.</p>
+					</a>
+				</div>
+				<div class="features__item">
+					<a href="#" class="features__inner">
+						<h2 class="features__header">Разработка документации П+РД</h2>
+						<p class="features__text">Наша компания предлагает Вам разработку проекта и рабочей документации. Данная документация позволит быстрее приступить к строительству или реконструкции объекта.</p>
+					</a>
+				</div>
+				<div class="features__item">
+					<a href="#" class="features__inner">
+						<h2 class="features__header">Проектирование промышленных объектов</h2>
+						<p class="features__text">Компания «BIG Constructions» выполняет проектирование промышленных зданий и предприятий (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="features__item">
+					<a href="#" class="features__inner">
+						<h2 class="features__header">Технологии моделирования Tekla Structures</h2>
+						<p class="features__text">Современное программное обеспечение позволяет нашим специалистам в кратчайшие сроки создавать сооружения любой сложности и производить точные расчеты.</p>
+					</a>
+				</div>				
+			</div>
+		</section>
+		<section class="adv">
+			<div class="adv__wrapper lcw">
+				<h2 class="section-header">Наши преимущества:</h2>
+				<div class="adv__container">
+					<div class="adv__item">
+						<div class="adv__inner">
+							<div class="adv__circle">
+								<div class="adv__img"></div>
+							</div>
+							<h2 class="adv__header">Специалисты</h2>
+							<p class="adv__text">Сотрудники нашей компании имеют многолетний опыт проектирования сооружений различной сложности.</p>
+						</div>
+					</div>
+					<div class="adv__item">
+						<div class="adv__inner">
+							<div class="adv__circle">
+								<div class="adv__img"></div>
+							</div>
+							<h2 class="adv__header">Технологии</h2>
+							<p class="adv__text">Tekla Structures является самой современной и надежной программой информационного моделирования зданий (BIM).</p>
+						</div>
+					</div>
+					<div class="adv__item">
+						<div class="adv__inner">
+							<div class="adv__circle">
+								<div class="adv__img"></div>
+							</div>
+							<h2 class="adv__header">ГОСТ ЕСКД</h2>
+							<p class="adv__text">Разрабатываемые нами чертежи строго соответствуют единой системе конструкторской документации.</p>
+						</div>
+					</div>
+					<div class="adv__item">
+						<div class="adv__inner">
+							<div class="adv__circle">
+								<div class="adv__img"></div>
+							</div>
+							<h2 class="adv__header">Сроки</h2>
+							<p class="adv__text">Гарантия четкого соблюдения сроков выполнения работ является нашей приоритетной задачей.</p>
+						</div>
+					</div>					
+				</div>
+			</div>
+		</section>
+	</main>
+</div>
+<?php get_footer(); ?>

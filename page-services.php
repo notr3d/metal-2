7de88c38f4c1b62065ad2284db826f5f00fd1d18
Александр Некрасов <lxmrrr@gmail.com>
2016-09-09 16:49:43 +0300

    services

---
+++ page-services.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+<div class="content-area">
+	<main class="site-main services">
+		<section class="service">
+			<div class="service__wrapper lcw">
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">Проектирование и разработка КМД</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">Проектирование и разработка КМ</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">Проектирование производственных объектов</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">Проектирование фундамента</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">Проектирование крыши или кровли</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>
+				<div class="service__item">
+					<a href="#" class="service__inner">
+						<div class="service__bg"></div>
+						<h2 class="service__header">3Д моделирование Tekla Structures</h2>
+						<p class="service__text">Мы также проектируем промышленные здания и предприятия (ангаров, складских комплексов, цехов) на основе индивидуальных решений.</p>
+					</a>
+				</div>				
+			</div>
+		</section>
+	</main>
+</div>
+<?php get_footer(); ?>

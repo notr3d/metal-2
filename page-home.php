3d1e0ef05faf68945fc60c5f561071c2c3994a5c
Александр Некрасов <lxmrrr@gmail.com>
2016-09-09 10:59:27 +0300

    callback

--- page-home.php
+++ page-home.php
@@ -29,6 +29,40 @@
 				</div>				
 			</div>
 		</section>
+		<section class="callback">
+			<div class="callback__wrapper lcw">
+				<div class="callback__container">
+					<h2 class="callback__header">Бесплатная оценка<br>проекта КМД</h2>
+					<form class="callback__form">
+						<div class="callback__col callback__col--left">
+							<div class="callback__item callback__item--name">
+								<input type="text" placeholder="Ваше имя:*" required>
+							</div>
+							<div class="callback__item callback__item--tel">
+								<input type="tel" placeholder="Ваш телефон:*" required>
+							</div>
+							<div class="callback__item callback__item--email">
+								<input type="email" placeholder="Ваш e-mail:*" required>
+							</div>
+							<div class="callback__item callback__item--textarea">
+								<textarea placeholder="Описание проекта"></textarea>
+							</div>
+						</div>
+						<div class="callback__col callback__col--right">
+							<div class="callback__item callback__item--file">
+								<label>
+									<input type="file">
+									<span>Прикрепить файл</span>
+								</label>
+							</div>
+							<div class="callback__item callback__item--submit">
+								<input type="submit" value="Оценить КМД">
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</section>
 		<section class="adv">
 			<div class="adv__wrapper lcw">
 				<h2 class="section-header">Наши преимущества:</h2>
